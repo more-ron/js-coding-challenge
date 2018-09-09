ce5b527d73367f0a6590aa8115dd42f426bd354a
Ronald Maravilla <more.ron.too@gmail.com>
2018-09-08 23:21:19 -0700

    Fool Amazon from throlling

--- app/jobs/retrieve_product_job.rb
+++ app/jobs/retrieve_product_job.rb
@@ -25,7 +25,8 @@ class RetrieveProductJob < ApplicationJob
 
   def process_product_detail
     product.data['name'] = product_page.css('#productTitle').text.strip
-    product.data['main_image'] = product_page.css('#imgTagWrapperId img').attr('src').value
+    img = product_page.css('#imgTagWrapperId img')
+    product.data['main_image'] = img.attr('src').value if img.present?
 
     product_details = product_details_rows.map do |row|
       label = row.css('td.label,th').text.strip
@@ -65,13 +66,18 @@ class RetrieveProductJob < ApplicationJob
 
   def extract_ranks_from_node(value_node)
     ranks = []
-    sub_categories = value_node.css('ul')
+    sub_categories = value_node.css('ul li,span span')
 
     sub_categories.remove
-    ranks << extract_rank_from_string(value_node.text.strip)
 
-    sub_categories.css('li').each do |li|
-      ranks << extract_rank_from_string(li.text.strip)
+    left_over = value_node.text.strip
+
+    if left_over.present?
+      ranks << extract_rank_from_string(left_over)
+    end
+
+    sub_categories.each do |item|
+      ranks << extract_rank_from_string(item.text.strip)
     end
 
     ranks
@@ -102,12 +108,37 @@ class RetrieveProductJob < ApplicationJob
 
   # @return [Tempfile]
   def product_page_html_file
-    @product_page_html_file ||= open(amazon_product_page_url)
+    @product_page_html_file ||= open(
+      amazon_product_page_url,
+      'accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+      'accept-language' => 'en-US,en;q=0.9',
+      'cookie' => amazon_cookie,
+      'referer' => Rails.cache.fetch('amazon-referer') { amazon_product_page_url },
+      'upgrade-insecure-requests' => '1',
+      'user-agent' => 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
+    )
+  ensure
+    # NOTE: Try and fool Amazon into thinking it's not a scraper.
+    Rails.cache.write('amazon-referer', amazon_product_page_url)
+
+    if @product_page_html_file.present?
+      set_cookie = @product_page_html_file.meta['set-cookie']
+
+      if set_cookie
+        Rails.cache.write('amazon-cookie', [amazon_cookie, set_cookie.split('; ', 2)[0]].join('; '))
+      end
+    end
+  end
+
+  def amazon_cookie
+    Rails.cache.fetch('amazon-cookie') do
+      'session-id=142-3890860-1146932; session-id-time=2082787201l; x-wl-uid=1BtZAYdZJiS8rhY0b2cLGjZAdbkvckhleAxU2ub1lfhDzgTZb4aotL//F4fZ8WjYJsIREPagMqnw=; ubid-main=132-9770205-5722210; session-token=KT6ZvaimgH7yPrylXxUaMkreSgj1YSYl5Aw1XxejfpHIgACnkIlnItKIGe+kxV/XpslRhpS/kOCz3c26GzEBiXyYXvZvLQUvLPniB5eQKWYQQPfxtbgs6S6r3W4oiVvRYjly5TFymtX7Dq2BL7pHreZuTGR/0ZLWFT4vxpQTFZ2H9coZqAM7+Qo8OwBVuBgI28fvYdtg+0UjpuBln9Lz+Ur8ROqWlbSP2xYAXI/tRN0oe3lPnrUowZbMcf0sXRMO; csm-hit=tb:s-J3JXKP3BEPE7PNXS5ZTA|1536472201902&adb:adblk_no'
+    end
   end
 
   # @return [String]
   def amazon_product_page_url
-    "https://www.amazon.com/dp/#{asin}"
+    @amazon_product_page_url ||= "https://www.amazon.com/dp/#{asin}"
   end
 
   # Warning: If there are more than one external request done for this job please make changes to
